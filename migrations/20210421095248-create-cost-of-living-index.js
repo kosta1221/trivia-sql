54f7cfefaf97d163ec63d84e66abfc61358b7057
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-04-21 21:22:23 +0300

    drop unwanted columns migration

--- migrations/20210421095248-create-cost-of-living-index.js
+++ migrations/20210421095248-create-cost-of-living-index.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('CostOfLivingIndices', {
+    await queryInterface.createTable('cost_of_living_indices', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -40,6 +40,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('CostOfLivingIndices');
+    await queryInterface.dropTable('cost_of_living_indices');
   }
 };
