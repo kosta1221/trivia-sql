a18a192a797bbc789e2486f5a6e209e79a86ee8e
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-04-21 11:56:30 +0300

    initialize sequelize, add dotenv configuration

---
+++ config/config.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+module.exports = {
+	development: {
+		username: "root",
+		password: DB_PASSWORD,
+		database: "restaurant",
+		host: "127.0.0.1",
+		dialect: "mysql",
+		define: {
+			underscored: true,
+		},
+	},
+	test: {
+		username: "root",
+		password: null,
+		database: "database_test",
+		host: "127.0.0.1",
+		dialect: "mysql",
+	},
+	production: {
+		username: "root",
+		password: null,
+		database: "database_production",
+		host: "127.0.0.1",
+		dialect: "mysql",
+	},
+};
