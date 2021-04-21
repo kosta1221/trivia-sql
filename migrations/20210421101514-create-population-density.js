8551e6abb3c12f91441813f2f0dd5bac005c2c85
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-04-21 13:36:20 +0300

    create PopulatiuonDensity model

---
+++ migrations/20210421101514-create-population-density.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('population_density', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      rank: {
+        type: Sequelize.STRING
+      },
+      country_or_dependent_territory: {
+        type: Sequelize.STRING
+      },
+      area_km2: {
+        type: Sequelize.STRING
+      },
+      area_mi2: {
+        type: Sequelize.STRING
+      },
+      population: {
+        type: Sequelize.STRING
+      },
+      density_popkm2: {
+        type: Sequelize.STRING
+      },
+      density_popmi2: {
+        type: Sequelize.STRING
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      population_source: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('population_density');
+  }
+};
---
+++ models/PopulationDensity.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class PopulationDensity extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  PopulationDensity.init({
+    rank: DataTypes.STRING,
+    country_or_dependent_territory: DataTypes.STRING,
+    area_km2: DataTypes.STRING,
+    area_mi2: DataTypes.STRING,
+    population: DataTypes.STRING,
+    density_popkm2: DataTypes.STRING,
+    density_popmi2: DataTypes.STRING,
+    date: DataTypes.DATE,
+    population_source: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'PopulationDensity',
+    tableName: 'population_density',
+    underscored: true,
+  });
+  return PopulationDensity;
+};
