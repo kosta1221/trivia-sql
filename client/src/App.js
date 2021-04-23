a989170e5db784367d8e0bf842382fc6a4c0dd6c
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-04-23 12:53:27 +0300

    homepage styles + initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,40 @@
 import "./styles/App.css";
 import { BrowserRouter } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		height: `100vh`,
+		display: `flex`,
+		justifyContent: `center`,
+	},
+	buttonsFlex: {
+		display: `flex`,
+		justifyContent: `center`,
+		flexDirection: "column",
+		width: "40vmax",
+	},
+	mainButton: {
+		margin: "1vh",
+	},
+}));
 
 function App() {
+	const classes = useStyles();
+
 	return (
-		<div className="App">
-			<BrowserRouter></BrowserRouter>
+		<div className={`App ${classes.root}`}>
+			<BrowserRouter>
+				<div className={classes.buttonsFlex}>
+					<Button className={classes.mainButton} variant="contained" color="primary">
+						Start Game
+					</Button>
+					<Button className={classes.mainButton} variant="contained" color="primary">
+						Leaderboards
+					</Button>
+				</div>
+			</BrowserRouter>
 		</div>
 	);
 }
