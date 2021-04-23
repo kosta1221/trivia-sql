d3d287b52e30be37683564de42cf588dca623e6e
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-04-23 19:28:10 +0300

    can generate questions for 1 specific template

--- routes/generate.js
+++ routes/generate.js
@@ -2,7 +2,86 @@ const { Router } = require("express");
 
 const generate = Router();
 
+const {
+	Country,
+	CrimeIndex,
+	Capital,
+	CostOfLivingIndex,
+	PopulationDensity,
+	QualityOfLifeIndex,
+	Sequelize,
+} = require("../models");
+const Op = Sequelize.Op;
+
 generate.get("/", (req, res, next) => {
+	Country.findAll({
+		order: Sequelize.literal("rand()"),
+		limit: 4,
+	})
+		.then((countries) => {
+			countries.map((country) => console.log(country.toJSON().name));
+
+			PopulationDensity.findAll({
+				where: {
+					[Op.or]: [
+						{
+							country_or_dependent_territory: {
+								[Op.and]: [
+									{ [Op.notLike]: `%(%${countries[0].toJSON().name}%)%` },
+									{
+										[Op.like]: `%${countries[0].toJSON().name}%`,
+									},
+								],
+							},
+						},
+						{
+							country_or_dependent_territory: {
+								[Op.and]: [
+									{ [Op.notLike]: `%(%${countries[1].toJSON().name}%)%` },
+									{
+										[Op.like]: `%${countries[1].toJSON().name}%`,
+									},
+								],
+							},
+						},
+						{
+							country_or_dependent_territory: {
+								[Op.and]: [
+									{ [Op.notLike]: `%(%${countries[2].toJSON().name}%)%` },
+									{
+										[Op.like]: `%${countries[2].toJSON().name}%`,
+									},
+								],
+							},
+						},
+						{
+							country_or_dependent_territory: {
+								[Op.and]: [
+									{ [Op.notLike]: `%(%${countries[3].toJSON().name}%)%` },
+									{
+										[Op.like]: `%${countries[3].toJSON().name}%`,
+									},
+								],
+							},
+						},
+					],
+				},
+			}).then((countriesPopulationDensity) => {
+				const onlyRelevantProps = countriesPopulationDensity.map((countryPD) => {
+					console.log(countryPD.toJSON());
+					return {
+						country_or_dependent_territory: countryPD.country_or_dependent_territory,
+						population: countryPD.population,
+					};
+				});
+				onlyRelevantProps.sort((a, b) => a.population - b.population);
+				for (const bulbul of onlyRelevantProps) {
+					console.log(bulbul);
+				}
+			});
+		})
+		.catch((err) => next(err));
+
 	res.json({
 		questionStr: "456",
 		option1: "opti45645on1",
