656fa7cccc04ff7d57b077ee425a371f14be4e13
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-04-23 20:31:13 +0300

    can generate question CONSISTENTLY for 1 specific template

--- routes/generate.js
+++ routes/generate.js
@@ -10,85 +10,106 @@ const {
 	PopulationDensity,
 	QualityOfLifeIndex,
 	Sequelize,
+	sequelize,
 } = require("../models");
 const Op = Sequelize.Op;
 
-generate.get("/", (req, res, next) => {
-	Country.findAll({
-		order: Sequelize.literal("rand()"),
-		limit: 4,
-	})
-		.then((countries) => {
-			countries.map((country) => console.log(country.toJSON().name));
+generate.get("/", async (req, res, next) => {
+	sequelize.options.logging = false;
 
-			PopulationDensity.findAll({
-				where: {
-					[Op.or]: [
-						{
-							country_or_dependent_territory: {
-								[Op.and]: [
-									{ [Op.notLike]: `%(%${countries[0].toJSON().name}%)%` },
-									{
-										[Op.like]: `%${countries[0].toJSON().name}%`,
-									},
-								],
-							},
-						},
-						{
-							country_or_dependent_territory: {
-								[Op.and]: [
-									{ [Op.notLike]: `%(%${countries[1].toJSON().name}%)%` },
-									{
-										[Op.like]: `%${countries[1].toJSON().name}%`,
-									},
-								],
-							},
-						},
-						{
-							country_or_dependent_territory: {
-								[Op.and]: [
-									{ [Op.notLike]: `%(%${countries[2].toJSON().name}%)%` },
-									{
-										[Op.like]: `%${countries[2].toJSON().name}%`,
-									},
-								],
-							},
-						},
-						{
-							country_or_dependent_territory: {
-								[Op.and]: [
-									{ [Op.notLike]: `%(%${countries[3].toJSON().name}%)%` },
-									{
-										[Op.like]: `%${countries[3].toJSON().name}%`,
-									},
-								],
-							},
-						},
-					],
-				},
-			}).then((countriesPopulationDensity) => {
-				const onlyRelevantProps = countriesPopulationDensity.map((countryPD) => {
-					console.log(countryPD.toJSON());
-					return {
-						country_or_dependent_territory: countryPD.country_or_dependent_territory,
-						population: countryPD.population,
-					};
-				});
-				onlyRelevantProps.sort((a, b) => a.population - b.population);
-				for (const bulbul of onlyRelevantProps) {
-					console.log(bulbul);
-				}
-			});
-		})
-		.catch((err) => next(err));
+	let countriesPopulationDensity = [];
+	let attempts = 0;
+
+	while (countriesPopulationDensity.length !== 4) {
+		attempts++;
+		console.log(`attempt no. ${attempts}:`);
+		const countries = await find4RandomCountries();
+
+		console.log("4 names from Countries table: ");
+		countries.map((country) => console.log(country.toJSON().name));
+		const countriesFormatted = countries.map((country) => country.toJSON());
+
+		countriesPopulationDensity = await findCorrespondingPD(countriesFormatted);
+	}
+
+	console.log("attempts: ", attempts);
+
+	const countriesPDMini = countriesPopulationDensity.map((countryPD) => {
+		return {
+			country_or_dependent_territory: countryPD.country_or_dependent_territory,
+			population: countryPD.population,
+		};
+	});
+	countriesPDMini.sort((a, b) => a.population - b.population);
+	for (const countryPDMini of countriesPDMini) {
+		console.log(countryPDMini);
+	}
+
+	sequelize.options.logging = true;
 
 	res.json({
-		questionStr: "456",
-		option1: "opti45645on1",
-		option2: "opti4564564on2",
-		option3: "opti645645on3",
-		option4: "opt6456456ion4",
+		questionStr: "placeholder",
+		option1: "option1",
+		option2: "option2",
+		option3: "option3",
+		option4: "option4",
 	});
 });
+
+const find4RandomCountries = async () => {
+	return Country.findAll({
+		order: Sequelize.literal("rand()"),
+		limit: 4,
+	});
+};
+
+const findCorrespondingPD = async (countries) => {
+	return PopulationDensity.findAll({
+		where: {
+			[Op.or]: [
+				{
+					country_or_dependent_territory: {
+						[Op.and]: [
+							{ [Op.notLike]: `%(%${countries[0].name}%)%` },
+							{
+								[Op.like]: `%${countries[0].name}%`,
+							},
+						],
+					},
+				},
+				{
+					country_or_dependent_territory: {
+						[Op.and]: [
+							{ [Op.notLike]: `%(%${countries[1].name}%)%` },
+							{
+								[Op.like]: `%${countries[1].name}%`,
+							},
+						],
+					},
+				},
+				{
+					country_or_dependent_territory: {
+						[Op.and]: [
+							{ [Op.notLike]: `%(%${countries[2].name}%)%` },
+							{
+								[Op.like]: `%${countries[2].name}%`,
+							},
+						],
+					},
+				},
+				{
+					country_or_dependent_territory: {
+						[Op.and]: [
+							{ [Op.notLike]: `%(%${countries[3].name}%)%` },
+							{
+								[Op.like]: `%${countries[3].name}%`,
+							},
+						],
+					},
+				},
+			],
+		},
+	});
+};
 
 module.exports = generate;
