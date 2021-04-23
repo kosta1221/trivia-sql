de85b039c495c911e827076b8f1c3f0367fa1b26
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-04-23 12:26:07 +0300

    initial react setup for client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./styles/App.css";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+	return (
+		<div className="App">
+			<BrowserRouter></BrowserRouter>
+		</div>
+	);
+}
+
+export default App;
