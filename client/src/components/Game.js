a7e282470022e39e9c74f9cd772ff8a35cad9975
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-04-23 14:40:31 +0300

    add buttons layout to question in game(non func)

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -11,13 +12,22 @@ const useStyles = makeStyles((theme) => ({
 	},
 	question: {
 		display: `flex`,
-		justifyContent: `center`,
+		justifyContent: `space-around`,
 		flexDirection: "column",
 		width: "60vmax",
 	},
-	mainButton: {
-		margin: "1vh",
-		width: "100%",
+	questionOptions: {
+		display: `flex`,
+		justifyContent: `center`,
+		flexDirection: "column",
+	},
+	questionOptionsInner: {
+		display: `flex`,
+		justifyContent: `center`,
+	},
+	optionButton: {
+		margin: "0.8vh",
+		flexGrow: 1,
 	},
 }));
 
@@ -28,6 +38,25 @@ function Game() {
 		<div className={classes.root}>
 			<Paper className={classes.question} elevation={3}>
 				Question goes here
+				<div className={classes.questionOptions}>
+					<div className={classes.questionOptionsInner}>
+						<Button className={classes.optionButton} variant="outlined" color="primary">
+							Option1
+						</Button>
+						<Button className={classes.optionButton} variant="outlined" color="primary">
+							Option2
+						</Button>
+					</div>
+
+					<div className={classes.questionOptionsInner}>
+						<Button className={classes.optionButton} variant="outlined" color="primary">
+							Option3
+						</Button>
+						<Button className={classes.optionButton} variant="outlined" color="primary">
+							Option4
+						</Button>
+					</div>
+				</div>
 			</Paper>
 		</div>
 	);
