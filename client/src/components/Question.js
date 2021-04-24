524714d8e5de66a2f5f3c8200ebd158c5833c40b
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-04-24 23:41:55 +0300

    add working TIMER for questions
    doesn't affect score & time, affects lives

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
+import LinearProgressWithLabel from "./LinearProgressWithLabel";
 
 const useStyles = makeStyles((theme) => ({
 	question: {
@@ -30,6 +31,31 @@ const useStyles = makeStyles((theme) => ({
 function Question({ data, setCurrentlyDisplayed, lives, setLives, score, setScore }) {
 	const classes = useStyles();
 
+	const [questionTime, setQuestionTime] = useState(20);
+	const [progress, setProgress] = useState(100);
+
+	const timerRef = useRef(null);
+
+	useEffect(() => {
+		timerRef.current = setInterval(() => {
+			setProgress((prevProgress) => (prevProgress <= 0 ? 100 : prevProgress - 0.5));
+			setQuestionTime((prevQuestionTime) => prevQuestionTime - 0.1);
+		}, 100);
+
+		return () => {
+			clearInterval(timerRef.current);
+		};
+	}, []);
+
+	useEffect(() => {
+		if (progress === 0) {
+			clearInterval(timerRef.current);
+			console.log("TIMES UP!!");
+			setCurrentlyDisplayed("rating");
+			setLives(lives - 1);
+		}
+	}, [progress]);
+
 	const handleAnswerClick = (event) => {
 		const isUserAnswerCorrect = event.target.innerText === data.answer ? true : false;
 		console.log(event.target.innerText);
@@ -44,6 +70,7 @@ function Question({ data, setCurrentlyDisplayed, lives, setLives, score, setScor
 
 	return (
 		<div className={classes.question}>
+			<LinearProgressWithLabel value={progress} questionTime={questionTime} />
 			{data.question_str}
 			<div className={classes.questionOptions}>
 				<div className={classes.questionOptionsInner}>
