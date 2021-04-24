808b69cebad183f536cea772b9db129c04d17574
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-04-24 17:42:30 +0300

    add Question component inside of Game

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -5,6 +5,7 @@ import Button from "@material-ui/core/Button";
 import { URL } from "../utils";
 
 import useAxios from "axios-hooks";
+import Question from "./Question";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -12,27 +13,6 @@ const useStyles = makeStyles((theme) => ({
 		height: `65vh`,
 		display: `flex`,
 		justifyContent: `center`,
-	},
-	question: {
-		display: `flex`,
-		justifyContent: `space-around`,
-		flexDirection: "column",
-		width: "60vmax",
-	},
-	questionOptions: {
-		display: `flex`,
-		justifyContent: `center`,
-		flexDirection: "column",
-	},
-	questionOptionsInner: {
-		display: `flex`,
-		justifyContent: `center`,
-	},
-	optionButton: {
-		textTransform: "none",
-		margin: "0.8vh",
-		width: "50%",
-		flexGrow: 1,
 	},
 }));
 
@@ -45,6 +25,8 @@ function Game() {
 		headers: { "Content-Type": "application/json" },
 	});
 
+	const [currentlyDisplayed, setCurrentlyDisplayed] = useState("question");
+
 	if (loading) {
 		return <h1>LOADING...</h1>;
 	}
@@ -54,31 +36,9 @@ function Game() {
 
 	return (
 		<div className={classes.root}>
-			<Paper className={classes.question} elevation={3}>
-				{data.question_str}
-				<div className={classes.questionOptions}>
-					<div className={classes.questionOptionsInner}>
-						<Button className={classes.optionButton} variant="outlined" color="primary">
-							{data.option1}
-						</Button>
-						<Button className={classes.optionButton} variant="outlined" color="primary">
-							{data.option2}
-						</Button>
-					</div>
-
-					<div className={classes.questionOptionsInner}>
-						{data.option3 && (
-							<Button className={classes.optionButton} variant="outlined" color="primary">
-								{data.option3}
-							</Button>
-						)}
-						{data.option4 && (
-							<Button className={classes.optionButton} variant="outlined" color="primary">
-								{data.option4}
-							</Button>
-						)}
-					</div>
-				</div>
+			<Paper elevation={3}>
+				{currentlyDisplayed === "question" && <Question data={data} />}
+				
 			</Paper>
 		</div>
 	);
---
+++ client/src/components/Question.js
@@ -0,0 +1,89 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+	question: {
+		height: "100%",
+		display: `flex`,
+		justifyContent: `space-around`,
+		flexDirection: "column",
+		width: "60vmax",
+	},
+	questionOptions: {
+		display: `flex`,
+		justifyContent: `center`,
+		flexDirection: "column",
+	},
+	questionOptionsInner: {
+		display: `flex`,
+		justifyContent: `center`,
+	},
+	optionButton: {
+		textTransform: "none",
+		margin: "0.8vh",
+		width: "50%",
+		flexGrow: 1,
+	},
+}));
+
+function Question({ data }) {
+	const classes = useStyles();
+
+	const handleAnswerClick = (event) => {
+		const isUserAnswerCorrect = event.target.innerText === data.answer ? true : false;
+		console.log(event.target.innerText);
+		console.log(isUserAnswerCorrect);
+	};
+
+	return (
+		<div className={classes.question}>
+			{data.question_str}
+			<div className={classes.questionOptions}>
+				<div className={classes.questionOptionsInner}>
+					<Button
+						onClick={handleAnswerClick}
+						className={classes.optionButton}
+						variant="outlined"
+						color="primary"
+					>
+						{data.option1}
+					</Button>
+					<Button
+						onClick={handleAnswerClick}
+						className={classes.optionButton}
+						variant="outlined"
+						color="primary"
+					>
+						{data.option2}
+					</Button>
+				</div>
+
+				<div className={classes.questionOptionsInner}>
+					{data.option3 && (
+						<Button
+							onClick={handleAnswerClick}
+							className={classes.optionButton}
+							variant="outlined"
+							color="primary"
+						>
+							{data.option3}
+						</Button>
+					)}
+					{data.option4 && (
+						<Button
+							onClick={handleAnswerClick}
+							className={classes.optionButton}
+							variant="outlined"
+							color="primary"
+						>
+							{data.option4}
+						</Button>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Question;
