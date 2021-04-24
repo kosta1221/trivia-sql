963d772e65b32b0e43e20bba2e196fce900cd3fa
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-04-24 18:30:58 +0300

    client can rate

--- client/src/components/Rating.js
+++ client/src/components/Rating.js
@@ -1,13 +1,32 @@
 import { IconButton } from "@material-ui/core";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StarOutlineIcon from "@material-ui/icons/StarOutline";
+import StarIcon from "@material-ui/icons/Star";
 
 function Rating() {
+	const [fullStars, setFullStars] = useState(0);
+
+	const handleStarHover = (event) => {
+		const value = event.target.getAttribute("value");
+		console.log(value);
+		setFullStars(value);
+	};
+
+	const handleStarClick = (event) => {
+		console.log(`${fullStars} stars`);
+	};
+
 	let fiveStars = [];
-	for (let i = 0; i < 5; i++) {
+	for (let i = 1; i <= 5; i++) {
 		fiveStars.push(
-			<IconButton key={`star-${i}`}>
-				<StarOutlineIcon />
+			<IconButton
+				onMouseEnter={handleStarHover}
+				onClick={handleStarClick}
+				value={i}
+				key={`star-${i}`}
+			>
+				{i <= fullStars && <StarIcon />}
+				{i > fullStars && <StarOutlineIcon />}
 			</IconButton>
 		);
 	}
