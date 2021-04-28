46f525f38d28757ab0012ea15ea393a41c314df4
michefraim <77235028+michefraim@users.noreply.github.com>
2021-04-28 18:49:45 +0300

    name is now the primary key of players, id column removed

--- migrations/20210422090648-create-player.js
+++ migrations/20210422090648-create-player.js
@@ -2,13 +2,8 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('players', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
       name: {
+        primaryKey: true,
         allowNull: false,  
         type: Sequelize.STRING
       },
