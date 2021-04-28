69d8698e9e97df59da3db9197795da1016ca6aab
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-04-28 20:07:34 +0300

    add /signup route instead of /save-player
    now each player will be unique

---
+++ routes/signup.js
@@ -0,0 +1,30 @@
+const { Router } = require("express");
+const bcrypt = require("bcrypt");
+
+const signup = Router();
+
+const { Player } = require("../models");
+
+signup.post("/", async (req, res, next) => {
+	const { playerName, password } = req.body;
+
+	const hashedPassword = await bcrypt.hash(password, 10);
+	console.log(hashedPassword);
+
+	const playerToSave = {
+		playerName,
+		password: hashedPassword,
+		avatar_id: null,
+		score: null,
+	};
+
+	try {
+		await Player.create(playerToSave);
+
+		res.status(201).send("success");
+	} catch (error) {
+		next(error);
+	}
+});
+
+module.exports = signup;
